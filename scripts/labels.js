16109d6f57a4d64ed188dcf96b789d28d329371a
Yann Gouffon <github@yago-art.ch>
2021-04-16 08:58:27 +0200

    Update labels.js

--- scripts/labels.js
+++ scripts/labels.js
@@ -1,134 +1,122 @@
 [{
-    name: 'EPIC',
-    color: '0052cc',
+    "name": "EPIC",
+    "color": "0052cc"
   },
   {
-    name: 'priority: Important',
-    color: 'ffa952',
+    "name": "priority: Important",
+    "color": "ffa952"
   },
   {
-    name: 'priority: Urgent',
-    color: 'ef5a5a',
+    "name": "priority: Urgent",
+    "color": "ef5a5a"
   },
   {
-    name: 'project: Change request',
-    color: 'ffa726',
+    "name": "project: Change request",
+    "color": "ffa726"
   },
   {
-    name: 'project: Client needed',
-    color: 'ffca28',
+    "name": "project: Client needed",
+    "color": "ffca28"
   },
   {
-    name: 'project: Estimate needed',
-    color: 'ffee58',
+    "name": "project: Estimate needed",
+    "color": "ffee58"
   },
   {
-    name: 'skill: Backend',
-    color: '90caf9',
+    "name": "skill: Backend",
+    "color": "90caf9"
   },
   {
-    name: 'skill: Devops',
-    color: '90caf9',
+    "name": "skill: Conception",
+    "color": "9fa8da"
   },
   {
-    name: 'skill: Conception',
-    color: '9fa8da',
+    "name": "skill: Design",
+    "color": "b39ddb"
   },
   {
-    name: 'skill: Design',
-    color: 'b39ddb',
+    "name": "skill: Frontend",
+    "color": "ce93d8"
   },
   {
-    name: 'skill: Frontend',
-    color: 'ce93d8',
+    "name": "skill: Marketing",
+    "color": "f48fb1"
   },
   {
-    name: 'skill: Marketing',
-    color: 'f48fb1',
+    "name": "skill: Writing",
+    "color": "c3938c"
   },
   {
-    name: 'status: Duplicate',
-    color: '4e342e',
+    "name": "status: Duplicate",
+    "color": "4e342e"
   },
   {
-    name: 'status: Help Wanted',
-    color: '424242',
+    "name": "status: Help Wanted",
+    "color": "424242"
   },
   {
-    name: "status: Won't fix",
-    color: '37474f',
+    "name": "status: Won't fix",
+    "color": "37474f"
   },
   {
-    name: 'time: 1',
-    color: 'a7c4e6',
+    "name": "time: 1",
+    "color": "a7c4e6"
   },
   {
-    name: 'time: 13',
-    color: '1f3c88',
+    "name": "time: 13",
+    "color": "1f3c88"
   },
   {
-    name: 'time: 2',
-    color: '7facdd',
+    "name": "time: 2",
+    "color": "7facdd"
   },
   {
-    name: 'time: 21',
-    color: '172c78',
+    "name": "time: 21",
+    "color": "172c78"
   },
   {
-    name: 'time: 3',
-    color: '5893d4',
+    "name": "time: 3",
+    "color": "5893d4"
   },
   {
-    name: 'time: 5',
-    color: '4576bb',
+    "name": "time: 5",
+    "color": "4576bb"
   },
   {
-    name: 'time: 8',
-    color: '3259a1',
+    "name": "time: 8",
+    "color": "3259a1"
   },
   {
-    name: 'time: to be estimated',
-    color: 'd3eeff',
+    "name": "time: to be estimated",
+    "color": "d3eeff"
   },
   {
-    name: 'time: ½',
-    color: 'ceddef',
+    "name": "time: ½",
+    "color": "ceddef"
   },
   {
-    name: 'type: Bug',
-    color: 'fd0054',
+    "name": "type: Bug",
+    "color": "fd0054"
   },
   {
-    name: 'type: Enhancement',
-    color: 'a6cb12',
+    "name": "type: Enhancement",
+    "color": "a6cb12"
   },
   {
-    name: 'type: Maintenance',
-    color: '75cac3',
+    "name": "type: Maintenance",
+    "color": "75cac3"
   },
   {
-    name: 'type: New feature',
-    color: '8293ff',
+    "name": "type: New feature",
+    "color": "8293ff"
   },
   {
-    name: 'type: Question',
-    color: 'ffcd3c',
+    "name": "type: Newsletter",
+    "color": "FB7D1D"
   },
   {
-    name: 'type: Question Design',
-    color: 'ffcd3c',
-  },
-  {
-    name: 'type: Question UX',
-    color: 'ffcd3c',
-  },
-  {
-    name: 'type: Review',
-    color: 'ffb480',
-  },
-  {
-    name: 'type: Styleguide',
-    color: '29b6f6',
+    "name": "type: Question",
+    "color": "ffcd3c"
   }
 ].forEach(function (label) {
   addLabel(label);
